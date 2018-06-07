05f7091ebd8cd83b204e48714e2e5bd706205f68
yuanyuan-w <w.y.yuan@foxmail.com>
2018-06-07 17:48:13 +0800

    feature done

--- EDA.py
+++ EDA.py
@@ -551,4 +551,9 @@ X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
 print("X_train and X_test Size: \n", X_train.shape, X_test.shape)
 
+print(X_train, X_test, y_train, y_test)
 
+np.save("./output/X_train.npy", X_train)
+np.save("./output/X_test.npy", X_test)
+np.save("./output/y_train.npy", y_train)
+np.save("./output/y_test.npy", y_test)
